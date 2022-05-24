f2f6e315e2a40d61454740779153e2ca9d6f7918
ryohei_kamei <ryohei_kamei@ryohei-kameinoMacBook-Air.local>
2022-05-24 14:49:49 +0900

    5/24

--- scripts/merge_all_into_master.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git checkout master
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
-git fetch --all
-git branch -r | grep -v '\->' | while read remote; do git merge --ff $remote; done
